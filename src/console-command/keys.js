89242846b33ed43354bbd3f27cf12eaf139eb817
Markus Tacker <m@coderbyheart.com>
2017-07-11 17:21:52 +0200

    fix(CLI): add switch to print private key

--- src/console-command/keys.js
+++ src/console-command/keys.js
@@ -3,11 +3,14 @@ import keys from '../services/keys'
 export default {
   name: 'keys',
   description: 'Print the private and public keys',
-  action: ({config, redis}) => {
+  options: [
+    ['-P, --private', 'Print the private key']
+  ],
+  action: ({config, redis}, options) => {
     return keys(config, redis.client)
       .then(({privateKey, publicKey}) => {
-        console.log(privateKey)
         console.log(publicKey)
+        if (options.private) console.log(privateKey)
       })
   }
 }
